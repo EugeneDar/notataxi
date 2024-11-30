a42b6bb02ac38bd1faebf151992100a3892b7f93
Sergei <sermir2003@gmail.com>
2024-11-30 07:07:46 +0300

    Improved the process of generating and deleting protobufs

---
+++ internal/proto/cleanup.sh
@@ -0,0 +1,12 @@
+set -e
+
+if [ ! -f LICENSE ]; then
+    echo "Most likely you are running the script from wrong directory, run from the root of the repository"
+    exit 1
+fi
+
+echo Deleting all automatically generated protobuf files...
+rm -rf internal/protobufs
+find . -type f -name '**.pb.go' -delete
+find . -type f -name '**_pb2.py' -delete
+find . -type f -name '**_pb2_grpc.py' -delete
